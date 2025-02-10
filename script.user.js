8c272d573b4ef4b83da8adb61612bdb8b8824747
Ferdinand Keller <ferdinand.keller@proton.me>
2025-02-10 16:49:37 +0100

    ads remover

--- script.user.js
+++ script.user.js
@@ -3,15 +3,15 @@
 // @namespace    https://ferdinandkeller.dev
 // @updateURL    https://raw.githubusercontent.com/ferdinandkeller/tampermonkey-asurascans/main/script.meta.js
 // @downloadURL  https://raw.githubusercontent.com/ferdinandkeller/tampermonkey-asurascans/main/script.user.js
-// @version      1.3
-// @description  Will limit the width of novels on Asura Comics website.
+// @version      1.4
+// @description  Will limit the width of novels on Asura Comics website & remove ads.
 // @author       Ferdinand Keller
 // @match        https://asuracomic.net/series/*/chapter/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
     // retrieve header
     let h2 = document.querySelector('h2')
@@ -20,4 +20,13 @@
     // edit the style of the div
     parent.style.maxWidth = '400px';
     parent.style.margin = '0 auto';
+
+    // find header ads banner and remove it
+    let header_ads = document.getElementById('header-ads')
+    let header_ads_next = header_ads.nextSibling
+    header_ads.remove()
+    header_ads_next.remove()
+
+    // find popup ad banner and remove it
+    document.getElementById('chapter-above-ads').nextSibling.children[1].children[0].remove()
 })();
