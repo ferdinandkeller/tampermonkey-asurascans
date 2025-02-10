121a91ed21b6cbac518bae7e67d67b2b283e1747
Ferdinand Keller <ferdinand.keller@proton.me>
2025-02-10 16:56:16 +0100

    make script compatible without server rendering

--- script.user.js
+++ script.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://ferdinandkeller.dev
 // @updateURL    https://raw.githubusercontent.com/ferdinandkeller/tampermonkey-asurascans/main/script.meta.js
 // @downloadURL  https://raw.githubusercontent.com/ferdinandkeller/tampermonkey-asurascans/main/script.user.js
-// @version      1.4
+// @version      1.4.1
 // @description  Will limit the width of novels on Asura Comics website & remove ads.
 // @author       Ferdinand Keller
 // @match        https://asuracomic.net/series/*/chapter/*
@@ -13,20 +13,33 @@
 
 (function () {
     'use strict';
-    // retrieve header
-    let h2 = document.querySelector('h2')
-    // retrieve the div containing the header
-    let parent = h2.parentElement
-    // edit the style of the div
-    parent.style.maxWidth = '400px';
-    parent.style.margin = '0 auto';
+
+    function wait_for_element_and_call(element_selector, callback) {
+        const interval_id = setInterval(() => {
+            const element = document.querySelector(element_selector);
+            if (element) {
+                clearInterval(interval_id);
+                callback(element);
+            }
+        }, 100);
+    }
+
+    // limit content max width
+    wait_for_element_and_call('h2', (h2) => {
+        let parent = h2.parentElement
+        parent.style.maxWidth = '400px';
+        parent.style.margin = '0 auto';
+    })
 
     // find header ads banner and remove it
-    let header_ads = document.getElementById('header-ads')
-    let header_ads_next = header_ads.nextSibling
-    header_ads.remove()
-    header_ads_next.remove()
+    wait_for_element_and_call('#header-ads', (header_ads) => {
+        let header_ads_next = header_ads.nextSibling
+        header_ads.remove()
+        header_ads_next.remove()
+    })
 
     // find popup ad banner and remove it
-    document.getElementById('chapter-above-ads').nextSibling.children[1].children[0].remove()
+    wait_for_element_and_call('#chapter-above-ads', (chapter_above_ads) => {
+        chapter_above_ads.nextSibling.children[1].children[0].remove()
+    })
 })();
